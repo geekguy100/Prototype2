5ef1b195e533eca71fa2dbd77408a207dcaba06c
darlynn-best <70346167+darlynn-best@users.noreply.github.com>
2020-10-18 20:42:49 -0500

    Sound

---
+++ Assets/Scripts/SoundBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBehaviour : MonoBehaviour
+{
+    public AudioSource click;
+    public void playSound(AudioSource click)
+    {
+        click.Play();
+    }
+}
