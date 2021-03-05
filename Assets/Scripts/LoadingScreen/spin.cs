5615b86404bfc2cf8157def75ffcebfc927d9b66
EinEstes <70347210+EinEstes@users.noreply.github.com>
2021-03-05 14:45:20 -0600

    Loading Screen Goofs And Gaffs

---
+++ Assets/Scripts/LoadingScreen/spin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    [Tooltip("the speed at which the object spin")]
+    public float spinSpeed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, spinSpeed * Time.deltaTime * 100, Space.World);
+    }
+}
