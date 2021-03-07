1b063387973bc42e8f1cbdd5c21e890cf78ccae3
awielgo <awielgos@mail.bradley.edu>
2021-03-06 20:54:40 -0600

    Implemented Money Assets, Figured out Title Screen

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,25 @@
+﻿/*****************************************************************************
+// File Name :         AnimationController.cs
+// Author :            Ashley Wielgos
+// Creation Date :     03/06/2021
+//
+// Brief Description : Holder for animation scripts.
+*****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
