9d3ec0b18580541decf02e91794cb2cef6da4e2b
awielgo <awielgos@mail.bradley.edu>
2021-03-07 19:18:20 -0600

    Test Push

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -11,10 +11,22 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
+    public int roombaPath;
+    Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
+        if (roombaPath == 1)
+        {
+            animator.SetBool("Tutorial", true);
+        }
+        else if (roombaPath == 2)
+        {
+            animator.SetBool("Conference", true);
+        }
     }
 
     // Update is called once per frame
