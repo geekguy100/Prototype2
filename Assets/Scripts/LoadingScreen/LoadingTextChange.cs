9e4fd642676a11b185862569c3b0acc35f44cbf7
EinEstes <70347210+EinEstes@users.noreply.github.com>
2021-03-13 15:32:42 -0600

    loading screen text change

---
+++ Assets/Scripts/LoadingScreen/LoadingTextChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadingTextChange : MonoBehaviour
+{
+    [SerializeField] public List<string> loadOutTitles;
+    [SerializeField] public List<string> loadOutHints;
+    public TextMeshProUGUI titleTextObject;
+    public TextMeshProUGUI hintTextObject;
+    private int lastLoadout = 0;
+
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        if (loadOutTitles.Count > 2)
+        {
+            int choice = Random.Range(0, loadOutTitles.Count);
+            
+
+            titleTextObject.text = loadOutTitles[choice];
+            hintTextObject.text = loadOutHints[choice];
+        }
+    }
+}
