c173ec9ff01af43ce9fb6d2bd6a465f23aa9860d
EinEstes <70347210+EinEstes@users.noreply.github.com>
2021-03-20 13:36:14 -0500

    Resolution settings

--- Assets/Scripts/LoadingScreen/SettingsMenu.cs
+++ Assets/Scripts/LoadingScreen/SettingsMenu.cs
@@ -2,20 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
+    Resolution[] resolutions;
     public AudioMixer music;
     public AudioMixer ui;
+    public TMP_Dropdown resolutionDropdown;
+    public List<string> options = new List<string>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+   
+        resolutionDropdown.ClearOptions();
+        resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+
+        foreach (var res in resolutions)
+        {
+            Debug.Log(res.width + "x" + res.height + " : " + res.refreshRate);
+        }
+
+        System.Collections.Generic.List<Resolution> list = new System.Collections.Generic.List<Resolution>(resolutions);
+        list.RemoveAt(0);
+        list.RemoveAt(0);
+        list.RemoveAt(0);
+        list.RemoveAt(0);
+        resolutions = list.ToArray();
+
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                currentResolutionIndex = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.RefreshShownValue();
     }
 
-    // Update is called once per frame
-    public void AdjustMasterVolume(float newVolume)
+        // Update is called once per frame
+        public void AdjustMasterVolume(float newVolume)
     {
         AudioListener.volume = newVolume;
     }
@@ -35,5 +69,12 @@ public class SettingsMenu : MonoBehaviour
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width,
+                  resolution.height, Screen.fullScreen);
     }
 }
