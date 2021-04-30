94891d57461e55c977bbed541d7287c11cb222bc
tcaron1024 <60328069+tcaron1024@users.noreply.github.com>
2021-04-30 17:46:47 -0500

    added in racetrack interaction

---
+++ Assets/Scripts/Objects/RacetrackController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacetrackController : Interactable
+{
+    [Tooltip("Audio clip to play when bad approval poster animates")]
+    public AudioClip playClip;
+
+    Animator anim;
+    public void Interact()
+    {
+        if (anim == null)
+        {
+            anim = GetComponent<Animator>();
+        }
+        // Change cursor to revealed one if it wasn't already
+        if (!revealed)
+        {
+            revealed = true;
+            ChangeCursor(revealed);
+        }
+        PlaySFX(playClip);
+        anim.SetTrigger("ShouldRun");
+    }
+}
