ab4501a0558ab893497e0e499d7c248b36d5585f
EinEstes <70347210+EinEstes@users.noreply.github.com>
2021-04-30 20:10:31 -0500

    Added cheerleader animation to loading screen and scripted it so both animations could appear

---
+++ Assets/Scripts/LoadingScreen/LoadingScreenAnimationSelection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreenAnimationSelection : MonoBehaviour
+{
+    public GameObject dance1;
+    public GameObject dance2;
+
+    private void OnEnable()
+    {
+        dance1.SetActive(false);
+        dance2.SetActive(false);
+
+        if(Random.value > 0.5f)
+        {
+            dance1.SetActive(true);
+        }
+        else
+        {
+            dance2.SetActive(true);
+        }
+    }
+}
